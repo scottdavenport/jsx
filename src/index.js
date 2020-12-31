715336f86f3e230db0efa7c1ee348ef6449db548
scottdavenport <scott@pinpoint.com>
2020-12-31 10:33:40 -0500

    Values JSX Can't Show

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-    const buttonText = 'Click me'
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter Name: ';
+
   return (
     <div>
       <label className="label" for="name">
-        Enter name:
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {buttonText}
+        {buttonText.text}
       </button>
     </div>
   );
