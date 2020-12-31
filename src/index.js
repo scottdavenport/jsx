0ce6195deff881fae344561087db35c315ba594c
scottdavenport <scott@pinpoint.com>
2020-12-31 09:50:27 -0500

    install prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-    return <div>Hi There!</div>
-}
+  return <div>Hi There!</div>;
+};
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
