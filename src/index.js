d1071c0534c8345b36d600258ab37339d6ee072d
scottdavenport <scott@pinpoint.com>
2020-12-31 10:27:04 -0500

    Referencing JS Variables in JSX - remove function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function getButtonText () {
-    return 'Click on me!'
-}
-
 // Create a react component
 const App = () => {
+    const buttonText = 'Click me'
   return (
     <div>
       <label className="label" for="name">
@@ -15,7 +12,7 @@ const App = () => {
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {getButtonText()}
+        {buttonText}
       </button>
     </div>
   );
